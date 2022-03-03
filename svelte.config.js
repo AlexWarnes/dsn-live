e08b9b89915d29196e9d9936b5b04807f110e034
Alex Warnes <alex.s.warnes@gmail.com>
2022-03-03 15:54:08 -0600

    add netlify toml

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-netlify';
+import adapter from '@sveltejs/adapter-auto';
 import preprocess from 'svelte-preprocess';
 import { optimizeImports } from "carbon-preprocess-svelte";
 
