525518c939096c5e23f1cceaefba50567bd09afe
Alex Warnes <alex.s.warnes@gmail.com>
2022-03-03 15:51:15 -0600

    add adapter-netlify

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-netlify';
 import preprocess from 'svelte-preprocess';
 import { optimizeImports } from "carbon-preprocess-svelte";
 
